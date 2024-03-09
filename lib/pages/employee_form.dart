6b49dba14bac574c51d2c5abc34a8d1339e4f6a9
ayushhhshh <2022bcacsbayush12896@poornima.edu.in>
2024-03-09 15:27:03 +0530

    Building

---
+++ lib/pages/employee_form.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class EmployeeForm extends StatelessWidget {
+  const EmployeeForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(onPressed: (){}, icon: const Icon(Icons.arrow_back)),
+        title: const Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("Employee",style: TextStyle(fontFamily: "Lato", fontWeight: FontWeight.bold,fontSize: 25, color: Colors.blue),),
+          Text("Form",style: TextStyle(fontFamily: "Lato", fontWeight: FontWeight.bold,fontSize: 25, color: Colors.orange),),
+        ],
+      ),),
+      body: Container(
+        margin: const EdgeInsets.all(15),
+        child:   Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text("Name", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 27),),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(8))),
+              ),
+            ),
+            const SizedBox(height: 15),
+            const Text("Age", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 27),),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextField(
+                decoration: InputDecoration(border: OutlineInputBorder(borderRadius: BorderRadius.circular(8))),
+              ),
+            ),
+            const SizedBox(height: 15),
+            const Text("Location", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 27),),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextField(
+                decoration: InputDecoration(border: OutlineInputBorder(borderRadius: BorderRadius.circular(8))),
+              ),
+            ),
+            const SizedBox(height: 40),
+            Center(child: ElevatedButton(onPressed: () {}, child: const Text("Add", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),))),
+          ],
+        ),
+        ),
+    );
+  }
+}
---
+++ lib/pages/home.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("Employees", style: TextStyle(fontFamily: "Lato", fontWeight: FontWeight.bold, color: Colors.blue),
+          ),
+          Text("Firebase", style: TextStyle(fontFamily: "Lato", fontWeight: FontWeight.bold, color: Colors.orange),
+          ),
+        ],
+      ),),
+      floatingActionButton: FloatingActionButton(onPressed: (){}, child: IconButton(onPressed: (){}, icon: GestureDetector(child: const Icon(Icons.add))),),
+      
+    );
+  }
+}
