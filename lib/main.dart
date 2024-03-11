f65507859c0f3a54e25360cdcb30469ef9ca2d1f
ayushhhshh <2022bcacsbayush12896@poornima.edu.in>
2024-03-11 23:54:58 +0530

    Building

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:employee_form/pages/employee_form.dart';
 import 'package:employee_form/pages/home.dart';
 import 'package:flutter/material.dart';
 
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: EmployeeForm(),
     );
   }
 }
