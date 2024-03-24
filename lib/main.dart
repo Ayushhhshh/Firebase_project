511d95b6d15ead969e656e2f3b31472cf25f3577
ayushhhshh <2022bcacsbayush12896@poornima.edu.in>
2024-03-24 19:46:30 +0530

    Firebase Addind

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:employee_form/pages/employee_form.dart';
 import 'package:employee_form/pages/home.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
-  // await Firebase.initializeApp(); 
-  // WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(); 
   runApp(const MyApp());
 }
 
